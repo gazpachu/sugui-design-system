d5ce611e1bbf8134077f144db717931e09f5a711
Joan Mira <JOAN.MIRA@VODAFONE.COM>
2019-01-30 22:47:10 +0100

    updated version

--- styleguide.config.js
+++ styleguide.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   title: 'SugUI Design System',
-  version: 'v1.0.0-alpha.3',
+  version: 'v1.0.0-alpha.5',
   theme: {
     color: {
       link: '#9C2AA0',
